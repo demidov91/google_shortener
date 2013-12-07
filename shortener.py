5ddf7f790d072d114c6d942634b82bccd2b0d720
dmitry <d.demidov@itransition.com>
2013-12-07 22:39:23 +0300

    Add *expand* function.

--- shortener.py
+++ shortener.py
@@ -5,12 +5,13 @@ Totally created by http://stackoverflow.com/a/17357552/1401639
 import urllib2
 import json
 
+API_URL = 'https://www.googleapis.com/urlshortener/v1/url'
+
 def shorten(url):
-    post_url = 'https://www.googleapis.com/urlshortener/v1/url'
     postdata = {'longUrl':url}
     headers = {'Content-Type':'application/json'}
     req = urllib2.Request(
-        post_url,
+        API_URL,
         json.dumps(postdata),
         headers
     )
@@ -24,3 +25,13 @@ the *shorten* function. Return **False** other way.
 """
 def is_short(url):
     return url.startswith('http://goo.gl/')
+
+
+def expand(url):
+    """
+    :url: short url to expand.
+    :returns: string, full url.
+    """
+    return json.loads(urllib2.urlopen(\
+        '{0}?shortUrl={1}'.format(API_URL, url)).\
+        read())['longUrl']
